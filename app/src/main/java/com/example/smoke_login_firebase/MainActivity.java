976751d0c95b9780c772aeaf5ac5ac2c4c5d687a
ANUSHKABHANDARY <anushkabhandary56@gmail.com>
2021-12-12 13:09:53 +0530

    second commit

--- app/src/main/java/com/example/smoke_login_firebase/MainActivity.java
+++ app/src/main/java/com/example/smoke_login_firebase/MainActivity.java
@@ -5,23 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button Next;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    public void logout(View view){
-        FirebaseAuth.getInstance().signOut();;
-        Intent intent = new Intent(getApplicationContext(),Login.class);
-        startActivity(intent);
-        finish();
+        Next = findViewById(R.id.bt1);
+
+        Next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+
+        });
+
     }
 }
